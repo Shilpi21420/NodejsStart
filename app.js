bf88746559926ca69d1b7fc1595f1fff2e5295a3
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-09-10 15:25:55 +0530

    Add files via upload
    parse

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const http = require('http');
+const http = require("http");
+const bodyParse = require("body-parser");
 
 const express = require("express");
 
 const app = express();
+app.use(bodyParse.urlencoded({ extended: false }));
 
-app.use((req,res,next) =>{
-    console.log("It is First Middleware");
-    next();//Allows the request  to continue to the next middleware in a line
-})
+// app.use((req,res,next) =>{
+//     console.log("It is First Middleware");
+//     next();//Allows the request  to continue to the next middleware in a line
+// })
 
-app.use((req,res,next) =>{
-    console.log("It is a second Middleware");
-    res.send("<h1>Hello World </h1>")
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='text'><button type='submit'>Submit</button></form>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
 
 app.listen(3000);
