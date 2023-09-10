4cf61d19856511c3a17913c26841219c23165050
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-09-10 14:38:09 +0530

    Add files via upload
    middleware

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const http = require("http");
-const  routes = require('./routes');
+const http = require('http');
 
-const server = http.createServer(routes);
-server.listen(3000);
+const express = require("express");
+
+const app = express();
+
+app.use((req,res,next) =>{
+    console.log("It is First Middleware");
+    next();//Allows the request  to continue to the next middleware in a line
+})
+
+app.use((req,res,next) =>{
+    console.log("It is a second Middleware");
+    res.send("<h1>Hello World </h1>")
+})
+
+app.listen(3000);
