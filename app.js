3a0eeac6ac7d7431a2f72edacbffd039510737f0
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-09-10 16:31:54 +0530

    Add files via upload
    Router file

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const http = require("http");
+const express = require('express');
 const bodyParse = require("body-parser");
 
-const express = require("express");
-
 const app = express();
+
+const adminrouter = require('./routes/admin');
+const shoprouter = require('./routes/shop');
+
+
 app.use(bodyParse.urlencoded({ extended: false }));
 
 // app.use((req,res,next) =>{
 //     console.log("It is First Middleware");
 //     next();//Allows the request  to continue to the next middleware in a line
 // })
+app.use('/admin',adminrouter);
+app.use(shoprouter);
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><input type='text'><button type='submit'>Submit</button></form>"
-  );
-});
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
