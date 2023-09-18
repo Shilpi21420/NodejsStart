6b4d504b08df15ba9ca3678b8599c5cf7e6d4177
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-09-18 19:41:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParse = require("body-parser");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminrouter = require('./routes/admin');
-const shoprouter = require('./routes/shop');
-const contactus = require('./routes/contactus');
-const successfulpage = require('./routes/success');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-
-app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req,res,next) =>{
-//     console.log("It is First Middleware");
-//     next();//Allows the request  to continue to the next middleware in a line
-// })
-app.use('/admin',adminrouter);
-app.use(shoprouter);
-app.use(contactus);
-app.use(successfulpage);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use((req,res,next) =>{
-    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
-})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,22 @@
 const path = require('path');
+
 const express = require('express');
 
-const rootdir = require('../util/path'); 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.sendFile(path.join(rootdir,"views", "shop.html"));
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
+});
 
-  module.exports = router;
+module.exports = router;
